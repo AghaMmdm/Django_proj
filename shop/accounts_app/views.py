2a0cd8c37407e9abaa3ebfdd5e1c546a362ee142
User <mrahimiwork83@gmail.com>
2025-06-25 21:22:30 +0330

    django template tags (vid16)

--- shop/accounts_app/views.py
+++ shop/accounts_app/views.py
@@ -1,15 +1,41 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
 
 # Create your views here.
 
-usernames = ["mahdi", "ali", "mohammad", "sadegh", "pooya"]
-
-def home(request):
-    return HttpResponse("this is accounts page")
+userss = [
+    {
+        'username' : 'mmd90',
+        'name' : 'mohammad',
+        'lastname' : 'rahimi',
+        'phone' : '09335336402',
+        'city' : 'esf'
+    },
+    {
+        'username' : 'ali90',
+        'name' : 'ali',
+        'lastname' : 'movahedi',
+        'phone' : '09335336402',
+        'city' : 'ysj'
+    },
+    {
+        'username' : 'mahdi90',
+        'name' : 'mahdi',
+        'lastname' : 'torki',
+        'phone' : '09335336402',
+        'city' : 'teh'
+    }
+]
 
 def users(request):
-    return HttpResponse("this is users page")
+    return render(request, 'accounts_app/users.html', context = {'users_list' : userss})
+
+def home(request):
+    return HttpResponse('this is accounts page')
+
 
 def profile(request, username):
-    return render(request, "accounts_app/profile.html", context= {"name" : username})
+    for tmp in userss:
+        if username == tmp['username']:
+            return render(request, 'accounts_app/profile.html', context= {'user' : tmp})
+    raise Http404('Invalid username')
