b6e62deadfd6bbe009350f0eb05e3357f0ab28b9
User <mrahimiwork83@gmail.com>
2025-07-01 21:35:11 +0330

    integer and boolean fields (vid23)

--- shop/courses_app/models.py
+++ shop/courses_app/models.py
@@ -6,6 +6,8 @@ from django.db import models
 class course(models.Model):
     title = models.CharField(max_length = 50)
     description = models.TextField()
+    status = models.BooleanField(default= False)
+    views = models.IntegerField(default= 0)
     
     def __str__(self):
         return self.title
