6c45277d1cd442178c6ce6562a9fd4c6bae1de09
User <mrahimiwork83@gmail.com>
2025-07-08 22:48:40 +0330

    untill static files2 (vid 26)

--- shop/courses_app/views.py
+++ shop/courses_app/views.py
@@ -6,3 +6,13 @@ from .models import course
 def courses_list(request):
     courses = course.objects.all()
     return render(request, "courses_app/courses.html", context = {'courses_list' : courses})
+
+def course_detail(request, id):
+    tmp = course.objects.get(id = id)
+    tmp.views += 1
+    if tmp.status == False:
+        tmp.status = True
+    else :
+        tmp.status = False
+    tmp.save()
+    return render(request, "courses_app/course_detail.html", context= { 'course' : tmp })
