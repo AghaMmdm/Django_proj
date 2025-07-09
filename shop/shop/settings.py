4746e3c41d2f2b227c7a5694909ed7af6858ec99
User <mrahimiwork83@gmail.com>
2025-07-09 19:33:01 +0330

    untill get method (vid 28)

--- shop/shop/settings.py
+++ shop/shop/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
+from os import path
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -120,6 +121,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [
+    path.join(BASE_DIR, 'assets')
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
